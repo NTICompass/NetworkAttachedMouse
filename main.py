c795255186128db64e0495d2f6a4ace9c2050a05
Eric Siegel <NTICompass@gmail.com>
2026-06-24 18:25:46 -0400

    fix `init()` to know it wants a `BaseDecoder`

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
-from asyncio import Protocol
 from collections.abc import Coroutine
+from decoders.basedecoder import BaseDecoder
+from decoders.microsoft import Microsoft
 from pynput.keyboard import Listener, Key
 from serial.serialutil import SerialException
 from serial_asyncio import SerialTransport
-from decoders.microsoft import Microsoft
 from typing import Any
 import asyncio
 import serial_asyncio
 
-def init[T:Protocol](factory: type[T]) -> Coroutine[Any, Any, tuple[SerialTransport, T]]:
-    # 'COM7', **opts
+def init[T:BaseDecoder](factory: type[T]) -> Coroutine[Any, Any, tuple[SerialTransport, T]]:
+    # 'COM7', **factory.options
     # 'socket://192.168.1.123:3008'
-    return serial_asyncio.create_serial_connection(loop, factory, 'socket://192.168.1.123:3008', timeout=5, **factory.options)
+    return serial_asyncio.create_serial_connection(loop, factory, 'socket://192.168.1.123:3008', timeout=5)
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
