a1db1a34a95faba5689e572654659cecdbb4a705
Eric Siegel <NTICompass@gmail.com>
2026-06-24 18:00:51 -0400

    press ESC to exit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+from asyncio import Protocol
 from collections.abc import Coroutine
-from typing import Any
+from pynput.keyboard import Listener, Key
+from serial.serialutil import SerialException
 from serial_asyncio import SerialTransport
 from sermouse.decoder import Decoder
+from typing import Any
 import asyncio
 import serial
 import serial_asyncio
-from asyncio import Protocol
 
 def init[T:Protocol](factory: type[T]) -> Coroutine[Any, Any, tuple[SerialTransport, T]]:
     opts = {
@@ -16,15 +18,30 @@ def init[T:Protocol](factory: type[T]) -> Coroutine[Any, Any, tuple[SerialTransp
     }
 
     # 'COM7', **opts
-    return serial_asyncio.create_serial_connection(loop, factory, 'socket://192.168.1.123:3008')
+    # 'socket://192.168.1.123:3008'
+    return serial_asyncio.create_serial_connection(loop, factory, 'socket://192.168.1.123:3008', timeout=5)
 
 if __name__ == '__main__':
     loop = asyncio.new_event_loop()
-    transport, protocol = loop.run_until_complete(init(Decoder))
 
     try:
-        loop.run_forever()
-    except KeyboardInterrupt:
-        print('Exit')
+        print('Connecting to serial mouse...')
+        transport, protocol = loop.run_until_complete(init(Decoder))
+    except SerialException as e:
+        print(e)
+        loop.stop()
+    else:
+        try:
+            print('Connected!')
+            print('Press ESC to exit')
+            Listener(
+                on_press=lambda key: loop.stop() if key == Key.esc else None
+            ).start()
+            loop.run_forever()
+        except KeyboardInterrupt:
+            print('Exit')
+            loop.stop()
+        finally:
+            loop.close()
     finally:
         loop.close()
